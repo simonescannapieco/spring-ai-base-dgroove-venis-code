b40a9f5f0763fd6e0f66fab67abb4127d27fe8d6
Simone Scannapieco <simone.scannapieco@realt.it>
2025-09-24 13:39:11 +0200

    Modification from branch related to JSON user format

--- src/main/java/it/venis/ai/spring/demo/services/GeminiFromClientServiceImpl.java
+++ src/main/java/it/venis/ai/spring/demo/services/GeminiFromClientServiceImpl.java
@@ -105,8 +105,8 @@ public class GeminiFromClientServiceImpl implements GeminiFromClientService {
         String responseString;
 
         try {
-            JsonNode jsonNode = objectMapper.readTree(chatResponse.replace("`","").replaceFirst("json",""));
-            responseString = jsonNode.get("definizione").asText();
+            JsonNode rootNode = objectMapper.readTree(chatResponse.replace("`","").replaceFirst("json",""));
+            responseString = objectMapper.writeValueAsString(rootNode);
 
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
