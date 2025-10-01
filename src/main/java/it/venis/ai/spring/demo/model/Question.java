bc6a96ce7efd42c177e8b9f368eb76f9e3f1b2b2
Simone Scannapieco <simone.scannapieco@realt.it>
2025-10-01 11:52:07 +0200

    Stub for LLM output controls

--- src/main/java/it/venis/ai/spring/demo/model/Question.java
+++ src/main/java/it/venis/ai/spring/demo/model/Question.java
@@ -1,3 +1,12 @@
 package it.venis.ai.spring.demo.model;
 
-public record Question(String question) {}
+import java.util.UUID;
+
+public record Question(UUID id, String question) {
+
+    public Question(String question) {
+
+        this(UUID.randomUUID(), question);
+
+    }
+}
