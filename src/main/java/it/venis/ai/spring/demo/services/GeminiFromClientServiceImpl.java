f633c5024b835d0071d297c1301b35ffa919aed1
Simone Scannapieco <simone.scannapieco@gmail.com>
2025-10-03 09:06:40 +0200

    Update GeminiFromClientServiceImpl.java

--- src/main/java/it/venis/ai/spring/demo/services/GeminiFromClientServiceImpl.java
+++ src/main/java/it/venis/ai/spring/demo/services/GeminiFromClientServiceImpl.java
@@ -148,17 +148,24 @@ public class GeminiFromClientServiceImpl implements GeminiFromClientService {
     @Value("classpath:templates/get-artifact-sentiment-prompt.st")
     private Resource artifactSentimentPrompt;
 
+    @Value("classpath:templates/get-artifact-sentiment-prompt.st")
+    private Resource artifactSentimentPrompt;
+
     @Override
     public Answer getSentimentForArtifact(ArtifactRequest artifactRequest) {
-        Sentiment chatResponse = this.chatClient.prompt()
+        
+        List<String> stopSequences = Stream.of(" ", "\n").collect(Collectors.toList());
+
+        String chatResponse = this.chatClient.prompt()
                 .options(ChatOptions.builder()
-                //.model("gemini-2.0-flash")
+                .model("gemini-2.0-flash")
                 .temperature(0.1)
-                .topP(1.0)
+                //.topP(1.0)
                 //.topK(30)
                 .maxTokens(10)
                 //.frequencyPenalty(0.1)
                 //.presencePenalty(0.1)
+                .stopSequences(stopSequences)
                 .build())
                 .user(u -> u.text(this.artifactSentimentPrompt)
                         .params(Map.of("recensione", artifactRequest.artifact().body(), "artefatto", artifactRequest.artifact().type())))
@@ -166,9 +173,10 @@ public class GeminiFromClientServiceImpl implements GeminiFromClientService {
                         .endDelimiterToken('}')
                         .build())
                 .call()
-                .entity(Sentiment.class);
+                .content();
+                //.entity(Sentiment.class);
 
-        return new Answer(chatResponse.getSentiment());
+        return new Answer(chatResponse);
 
     }
 
