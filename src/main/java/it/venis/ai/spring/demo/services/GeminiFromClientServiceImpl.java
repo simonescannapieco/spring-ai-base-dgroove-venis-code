1edf5bdf31f0fb09822165522fd2a9b46b664874
Simone Scannapieco <simone.scannapieco@realt.it>
2025-10-08 15:18:13 +0200

    Stub for implicit prompting

--- src/main/java/it/venis/ai/spring/demo/services/GeminiFromClientServiceImpl.java
+++ src/main/java/it/venis/ai/spring/demo/services/GeminiFromClientServiceImpl.java
@@ -26,6 +26,7 @@ import it.venis.ai.spring.demo.model.ArtifactRequest;
 import it.venis.ai.spring.demo.model.DefinitionRequest;
 import it.venis.ai.spring.demo.model.DefinitionResponse;
 import it.venis.ai.spring.demo.model.Question;
+import it.venis.ai.spring.demo.model.TranslationRequest;
 
 @Service
 public class GeminiFromClientServiceImpl implements GeminiFromClientService {
@@ -147,6 +148,36 @@ public class GeminiFromClientServiceImpl implements GeminiFromClientService {
 
         return converter.convert(Objects.requireNonNull(chatResponse));
 
+    }
+
+    @Value("classpath:templates/get-lemma-translation-prompt.st")
+    private Resource lemmaTanslationPrompt;
+
+    @Override
+    public Answer getTranslationForLemma(TranslationRequest translationRequest) {
+        
+        List<String> stopSequences = Stream.of(" ", "|", "\n").collect(Collectors.toList());
+
+        String chatResponse = this.chatClient.prompt()
+                .options(ChatOptions.builder()
+                .model("gemini-2.0-flash")
+                .temperature(0.1)
+                //.topP(1.0)
+                //.topK(30)
+                .maxTokens(20)
+                //.frequencyPenalty(0.1)
+                //.presencePenalty(0.1)
+                .stopSequences(stopSequences)
+                .build())
+                .user(u -> u.text(this.lemmaTanslationPrompt)
+                        .params(Map.of("lemma", translationRequest.lemma())))
+                .templateRenderer(StTemplateRenderer.builder().startDelimiterToken('{')
+                        .endDelimiterToken('}')
+                        .build())
+                .call()
+                .content();
+
+        return new Answer(chatResponse);
     }
 
     @Value("classpath:templates/get-artifact-sentiment-prompt.st")
